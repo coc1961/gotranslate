aaf1b6667fce502eef89e48c6a28ded1bbbfb524
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2020-01-30 21:12:45 -0300

    Optimize translate process

--- internal/translate/translate.go
+++ internal/translate/translate.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/printer"
 	"go/token"
@@ -34,6 +35,47 @@ type translate struct {
 	language       string
 	inPath         string
 	outPath        string
+	files          []source
+}
+
+type source struct {
+	t       *translate
+	fileSet *token.FileSet
+	node    *ast.File
+	path    string
+	comment []*ast.Comment
+}
+
+func (s source) save() error {
+	b := bytes.Buffer{}
+	for _, s := range s.comment {
+		b.Write([]byte(s.Text + "\n\r\r\n"))
+	}
+	str := b.String()
+
+	if txt, err := google_translate(str, s.t.sourceLanguage, s.t.language); err != nil {
+		return err
+	} else {
+		arr := strings.Split(txt, "\n\r\r\n")
+		if len(arr)-1 == len(s.comment) {
+			for i := 0; i < len(s.comment); i++ {
+				s.comment[i].Text = strings.ReplaceAll(strings.ReplaceAll(arr[i], "* /", "*/"), "/ *", "/*")
+				//fmt.Println(s.comment[i].Text, "===", arr[i])
+			}
+		}
+	}
+
+	if err := os.MkdirAll(filepath.Dir(s.path), 0700); err != nil {
+		return err
+	}
+
+	if fo, err := os.Create(s.path); err == nil {
+		printer.Fprint(fo, s.fileSet, s.node)
+		defer fo.Close()
+	} else {
+		return err
+	}
+	return nil
 }
 
 func (t *translate) validate() (*translate, error) {
@@ -51,9 +93,17 @@ func (t *translate) validate() (*translate, error) {
 }
 
 func (t *translate) Translate() error {
+	t.files = make([]source, 0)
 	var err error
 	if err = filepath.Walk(t.inPath, t.visit); err != nil {
 		return err
+	}
+
+	for _, f := range t.files {
+		fmt.Println("Processing file " + f.path)
+		if err := f.save(); err != nil {
+			return nil
+		}
 	}
 	return nil
 }
@@ -78,26 +128,25 @@ func (t *translate) visit(path string, f os.FileInfo, err1 error) error {
 		return fmt.Errorf("parseFile error:%+v", err)
 	}
 
+	var src source
+	src.comment = make([]*ast.Comment, 0)
+
 	for _, c := range node.Comments {
 		for _, l := range c.List {
-			if s, err := t.google_translate(l.Text, t.sourceLanguage, t.language); err == nil {
-				l.Text = s
-			} else {
-				return err
+			if len(l.Text) > 0 {
+				src.comment = append(src.comment, l)
 			}
 		}
 	}
 
-	pt := filepath.Join(t.outPath, path)
-	if err := os.MkdirAll(filepath.Dir(pt), 0700); err != nil {
-		return err
-	}
+	if len(src.comment) > 0 {
+		pt := filepath.Join(t.outPath, path)
 
-	if fo, err := os.Create(pt); err == nil {
-		printer.Fprint(fo, fset, node)
-		defer fo.Close()
-	} else {
-		return err
+		src.t = t
+		src.path = pt
+		src.fileSet = fset
+		src.node = node
+		t.files = append(t.files, src)
 	}
 
 	return nil
@@ -109,7 +158,7 @@ type post struct {
 	Target string `json:"target"`
 }
 
-func (t *translate) google_translate(text, source, target string) (string, error) {
+func google_translate(text, source, target string) (string, error) {
 	endpoint := "https://script.google.com/macros/s/AKfycbywwDmlmQrNPYoxL90NCZYjoEzuzRcnRuUmFCPzEqG7VdWBAhU/exec"
 
 	postData, err := json.Marshal(post{text, source, target})
